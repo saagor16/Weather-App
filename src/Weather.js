55c8192332923843fba29a3032653e86dda1e4df
saagor16 <saagor.16@gmail.com>
2025-01-16 12:12:46 +0600

    project e tailwine css setup

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Weather() {
+  return (
+    <div>
+       <div className="text-center bg-blue-500 text-white py-4">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+      <p className="mt-2">This is a Tailwind CSS setup in React.</p>
+    </div>
+    </div>
+  )
+}
+
+export default Weather
