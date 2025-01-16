27b6d368717a6de09aaa9cfbf00f248cfc5b81d9
saagor16 <saagor.16@gmail.com>
2025-01-16 12:21:19 +0600

    api fetch korar jonno axios i

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Weather() {
+    const[city,setCity]=useState();
+    const handleCityChange = (event)=>{
+        setCity(event.target.value)
+    }
+    const handleClick =()=>{
+        
+    }
   return (
-    <div>
-       <div className="text-center bg-blue-500 text-white py-4">
-      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
-      <p className="mt-2">This is a Tailwind CSS setup in React.</p>
-    </div>
+    <div className=''>
+      <input text='text' placeholder='Enter City Name' value={city} onChange={handleCityChange}></input>
+      <button onClick={handleClick}>Get Weather</button>
     </div>
   )
 }
